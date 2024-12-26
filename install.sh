1ceb1d473aa11a586604d06e49b7612ce363cc0b
emthompson <emthompson@usgs.gov>
2024-12-26 16:52:50 -0700

    Put matplotlibrc stuff back

--- install.sh
+++ install.sh
@@ -15,8 +15,10 @@ usage()
 unamestr=`uname`
 if [ "$unamestr" == 'Linux' ]; then
     prof=~/.bashrc
+    matplotlibdir=~/.config/matplotlib
 elif [ "$unamestr" == 'FreeBSD' ] || [ "$unamestr" == 'Darwin' ]; then
     prof=~/.bash_profile
+    matplotlibdir=~/.matplotlib
 else
     echo "Unsupported environment. Exiting."
     exit
@@ -58,6 +60,32 @@ echo "######Location of conda install: ${CONDA_LOC}"
 # Are we in an environment
 CURRENT_ENV=`conda info --envs | grep "*"`
 echo "Current conda environment: ${CURRENT_ENV}"
+
+# create a matplotlibrc file with the non-interactive backend "Agg" in it.
+if [ ! -d "$matplotlibdir" ]; then
+    mkdir -p $matplotlibdir
+    # if mkdir fails, bow out gracefully
+    if [ $? -ne 0 ];then
+        echo "Failed to create matplotlib configuration file. Exiting."
+        exit 1
+    fi
+fi
+
+matplotlibrc=$matplotlibdir/matplotlibrc
+if [ ! -e "$matplotlibrc" ]; then
+    echo "backend : Agg" > "$matplotlibrc"
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+elif grep -Fxq "backend : Agg" $matplotlibrc ; then
+    :
+elif [ ! grep -Fxq "backend" $matplotlibrc ]; then
+    echo "backend : Agg" >> $matplotlibrc
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+else
+    sed -i '' 's/backend.*/backend : Agg/' $matplotlibrc
+    echo "###############"
+    echo "NOTE: $matplotlibrc has been changed to set 'backend : Agg'"
+    echo "###############"
+fi
 
 # Is conda installed?
 conda --version
